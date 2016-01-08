81a8ab188393b05f84610b04565f6973b704854e
heys1988 <yuesheng66@yeah.net>
2016-01-08 15:47:32 +0800

    The first time for excel2sql

---
+++ webroot/excel2sql/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!--<meta http-equiv=”refresh” content=”3;url=example.php”>-->
+        <title>excel2sql</title>
+    </head>
+    <body style="background-color: #cce8cf">
+    <center>        
+        <form id="filedetail" name="filedetail" enctype="multipart/form-data">
+            <fieldset>
+                <legend>Shenzhen Giec Electronics Co., Ltd.</legend>
+                <label for="xlsfile">Select excel file(*.xls): </label>
+                <input type="file" id="xlsfile" name="xlsfile" ></input><br />
+                <input type="submit" id="submit1" name="submit1" value="Upload" />
+                <input type="reset" id="reset1" name="reset1" value="Reset" />
+                </fieldset>
+        </form>
+    </center>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
