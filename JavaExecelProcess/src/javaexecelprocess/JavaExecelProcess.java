a7c8e16d1a9df815dc33c0fd30d94da58c001dce
heys1988 <yuesheng66@yeah.net>
2016-01-13 18:02:37 +0800

    bugs#1

--- JavaExecelProcess/src/javaexecelprocess/JavaExecelProcess.java
+++ JavaExecelProcess/src/javaexecelprocess/JavaExecelProcess.java
@@ -10,7 +10,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFRow;
 
 /**
  *
@@ -21,15 +23,74 @@ public class JavaExecelProcess {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String[] args) {
-//        // TODO code application logic here
-//        System.out.println("Hello excel process.");
-//    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello excel process.");
+        JavaExecelProcess inst = new JavaExecelProcess();
+        inst.str1 = inst.readExcel("新建 Microsoft Office Excel 工作表.xls");
+        System.out.println(inst.str1);
+    }
     private String str1 = null;
-    public String processExcel(){
+    
+//    private String fileInPath = "~/ubgit/webroot/excel2sql/upload/新建 Microsoft Office Excel 工作表.xls";
+//    private String fileInPath = "/home/jupiter/ubgit/webroot/excel2sql/upload/新建 Microsoft Office Excel 工作表.xls";
+    private String fileInPath = "/home/jupiter/ubgit/webroot/excel2sql/upload/";
+    private HSSFWorkbook wb = null;
+    public String readExcel(String fileName){
+//        System.out.println("Hello excel process.");
+//        try{
+//            fileName = URLEncoder.encode(fileName, "UTF-8");
+//        }catch(UnsupportedEncodingException e){
+//            e.printStackTrace();
+//        }
+        
+        str1 = "Read "+fileName+" finished!";
+        fileInPath += fileName;
+        FileInputStream fisInFile = null;
+        
+        try {
+            fisInFile = new FileInputStream(fileInPath);
+            wb = new HSSFWorkbook(fisInFile);
+            fisInFile.close();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        str1 = str1 + " " + getSheets() + " "+ getRows() + " " + getColumns();
+        return str1;
+    }
+    
+    public int getSheets(){
+        int ret = 0;
+        ret = wb.getNumberOfSheets();
+        return ret;
+    }
+    public int getRows(){
+        int ret = 0;
+        HSSFSheet activeSheet = wb.getSheetAt(0);
+        ret = activeSheet.getPhysicalNumberOfRows();
+//        ret = activeSheet.getLastRowNum() - activeSheet.getFirstRowNum() + 1;
+//        ret = activeSheet.getNumMergedRegions();
+//        CellRangeAddress cra = activeSheet.getMergedRegion(0);
+//        ret = cra.getLastRow() - cra.getFirstRow() + 1;
+        
+        return ret;
+    }
+    public int getColumns(){
+        int ret = 0;
+        HSSFSheet activeSheet = wb.getSheetAt(0);
+        HSSFRow row1 = activeSheet.getRow(0);
+        ret = row1.getPhysicalNumberOfCells();
+//        CellRangeAddress cra = activeSheet.getMergedRegion(0);
+//        ret = cra.getLastColumn() - cra.getFirstColumn() + 1;
+        
+        return ret;
+    }
+    public String processExcel(String fileName){
 //        System.out.println("Hello excel process.");
         str1 = "Hello excel process second time.";
-        String strFileInPath = "/home/jupiter/ubgit/webroot/excel2sql/upload/杰科电子通讯录.xls";
+        String strFileInPath = "webroot/excel2sql/upload/";
+        strFileInPath += fileName;
         FileInputStream fisInFile = null;
         HSSFWorkbook wb = null;	
         try {
@@ -42,7 +103,8 @@ public class JavaExecelProcess {
         }
 			
         FileOutputStream fosFileOut = null;
-        String strFileOutPath = "/home/jupiter/ubgit/webroot/excel2sql/upload/result-杰科电子通讯录.xls";
+        String strFileOutPath = "webroot/excel2sql/upload/";
+        strFileOutPath += "result-"+fileName;
         File fileOut = new File(strFileOutPath);
         if(fileOut.exists()){
             fileOut.delete();
--- webroot/excel2sql/file_process.php
+++ webroot/excel2sql/file_process.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type: text/html; charset=utf-8");
 require_once("http://localhost:8080/javabridge/java/Java.inc");
 //require_once("../javabridge/META-INF/java/Java.inc");
 //require_once("http://localhost:8080/JavaBridge/java/Java.inc");
@@ -16,7 +17,11 @@ for($i=0; $i<$n; $i++) {
 $str = new java("java.lang.String", $s);
 echo '<br/>'.$str.'<br/>';
 $excel_prcess = new java("javaexecelprocess.JavaExecelProcess");
-$ret = $excel_prcess->processExcel();
-echo $ret;
+$ret = $excel_prcess->readExcel("新建 Microsoft Office Excel 工作表.xls");
+echo $ret.'<br/>';
+$ret = $excel_prcess->getRows();
+echo $ret.'<br/>';
+$ret = $excel_prcess->getColumns();
+echo $ret.'<br/>';
 echo '</center>';
 ?> 
